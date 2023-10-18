8f14e9bdd78d0e5ef64717f90e8c933eb5a3aca3
Elena Pronichkina <prolena39@mail.ru>
2023-10-18 22:37:57 +0300

    add code and page model

--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'catalog/index.html')
+
+
+def index_contacts(request):
+    return render(request, 'catalog/index_contacts.html')
