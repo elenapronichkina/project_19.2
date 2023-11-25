41241cb0c6e3aec9b1c1081fca905d12ff9d1019
Elena Pronichkina <prolena39@mail.ru>
2023-11-25 20:48:55 +0300

    hw21.1_add class

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, index_contacts, product, index_product
+from catalog.views import index, index_contacts, ProductListView, CategoryListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', index),
     path('contacts/', index_contacts),
-    path('products/', index_product),
-    path('product/<int:pk>/', product, name='product'),
+    path('products/', ProductListView.as_view(), name='index_product'),
+    path('product/<int:pk>/', CategoryListView.as_view(), name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Category, Product
 
@@ -20,19 +21,28 @@ def base_products(request):
     return render(request, 'catalog/base_products.html')
 
 
-def index_product(request):
-    context = {
-        'object_list': Category.objects.all(),
-        'title': 'КАТАЛОГ'
-    }
-    return render(request, 'catalog/index_product.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/index_product.html'
 
+# def index_product(request):
+#     context = {
+#         'object_list': Category.objects.all(),
+#         'title': 'КАТАЛОГ'
+#     }
+#     return render(request, 'catalog/index_product.html', context)
 
-def product(request, pk):
-    category = Category.objects.get(pk=pk)
-    data = {
-        'object_list': Product.objects.filter(category2=pk),
-        'title': f'продукты категории {category.name}',
-    }
-    return render(request, 'catalog/product.html', data)
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'catalog/product.html'
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = 'catalog/product.html'
+# def product(request, pk):
+#     category = Category.objects.get(pk=pk)
+#     data = {
+#         'object_list': Product.objects.filter(category2=pk),
+#         'title': f'продукты категории {category.name}',
+#     }
+#     return render(request, 'catalog/product.html', data)
 
